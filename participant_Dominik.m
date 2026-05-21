9e463599ee0a060ed8bdfad06246ff6b88d2b9f7
ma1320 <ma1320@nb-ma1320>
2026-05-21 14:17:29 +0200

    Comment New

--- participant_Dominik.m
+++ participant_Dominik.m
@@ -1,11 +1,11 @@
 function data = participant_Dominik()
 % PARTICIPANT_EXAMPLE  Vorlage für Teilnehmer-Dateien
-%  
+%   
 %   
 %   ANLEITUNG:
 %   1. Diese Datei kopieren als:  participant_[euer_vorname].m
 %      Beispiel: participant_anna.m
-%
+%   I want my comment
 %   2. Den Funktionsnamen anpassen:
 %      function data = participant_[euer_vorname]()
 %
